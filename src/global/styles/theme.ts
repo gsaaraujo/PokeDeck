9b32cf36c9c64e5e0384b78950a2d7662be2e0bd
Gabriel Araujo <gabriel.houth@gmail.com>
2021-07-18 15:31:51 -0300

    style: fonts replaced

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,22 @@
+export const theme = {
+  colors: {
+    decks: '#5B95CA',
+    button: '#F95B5B',
+
+    background: '#FFFFFF',
+
+    heading100: '#464646',
+    heading60: '#818490',
+    buttonTitle: '#F3F3F3',
+
+    highlight: '#F95B5B',
+    placeholder: '#A8B3E9',
+
+    header: '#FFD861',
+    bottomTab: '#FFD861',
+  },
+
+  fonts: {
+    buttonTitle: '',
+  },
+};
