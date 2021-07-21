0e7bb181c42aa85723d76f1a9ad58c8e8af3d021
Gabriel Araujo <gabriel.houth@gmail.com>
2021-07-21 13:46:40 -0300

    feat: add show/hide password into Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,36 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextInputProps } from 'react-native';
 
 import { theme } from '../../global/styles/theme';
 
-import { Container } from './styles';
+import EyeOnSVG from '../../assets/images/eye.svg';
+import EyeOffSVG from '../../assets/images/eye-off.svg';
+
+import { Container, TextInput, IconWrapper } from './styles';
 
 type Props = TextInputProps & {
   title: string;
   hasText: boolean;
+  hasIcon?: boolean;
+  isShowPassword?: boolean;
 };
 
-export const Input = ({ title, hasText, ...rest }: Props) => {
+export const Input = ({
+  title,
+  hasText,
+  hasIcon = false,
+  isShowPassword = false,
+  ...rest
+}: Props) => {
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
+  const [isEyeOn, setIsEyeOn] = useState(true);
+
+  useEffect(() => {
+    setIsEyeOn(isShowPassword);
+  }, []);
 
   const { highlight, placeholder } = theme.colors;
 
   const handleIsFocused = () => setIsFocused(!isFocused);
+
   const handleIsFilled = (inputText: boolean) => {
     setIsFocused(!isFocused);
 
     return setIsFilled(inputText);
   };
 
+  const handleIsEyeOn = () => setIsEyeOn(!isEyeOn);
+
   return (
-    <Container
-      placeholder={title}
-      color={isFocused || isFilled ? highlight : placeholder}
-      onFocus={handleIsFocused}
-      onBlur={() => handleIsFilled(hasText)}
-      {...rest}>
-      <></>
+    <Container>
+      <TextInput
+        placeholder={title}
+        color={isFocused || isFilled ? highlight : placeholder}
+        onFocus={handleIsFocused}
+        onBlur={() => handleIsFilled(hasText)}
+        secureTextEntry={isEyeOn}
+        {...rest}
+      />
+      {hasIcon && (
+        <IconWrapper onPress={handleIsEyeOn}>
+          {isEyeOn ? <EyeOffSVG /> : <EyeOnSVG />}
+        </IconWrapper>
+      )}
     </Container>
   );
 };
--- src/components/Input/styles.tsx
+++ src/components/Input/styles.tsx
@@ -7,13 +7,24 @@ type Props = {
   color: string;
 };
 
-export const Container = styled.TextInput.attrs<TextInputProps>({
+export const Container = styled.View`
+  width: 100%;
+`;
+
+export const TextInput = styled.TextInput.attrs<TextInputProps>({
   placeholderTextColor: theme.colors.placeholder,
   paddingVertical: 5,
 })<Props>`
   font-size: 18px;
+  font-family: ${theme.fonts.placeholder};
   margin-bottom: 55px;
   border-bottom-width: 2px;
   border-bottom-color: ${props => props.color};
-  font-family: ${theme.fonts.placeholder};
+`;
+
+export const IconWrapper = styled.TouchableOpacity`
+  top: -15px;
+  padding: 15px;
+  position: absolute;
+  align-self: flex-end;
 `;
