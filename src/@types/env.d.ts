0128819d736016fa9e16c8813a5dc80aad018e88
Gabriel Araujo <gabriel.houth@gmail.com>
2021-07-23 21:13:04 -0300

    feat: add more types for env

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,3 +1,7 @@
 declare module 'react-native-dotenv' {
   export const DB_API_URL: string;
+  export const CLIENT_ID: string;
+  export const RESPONSE_TYPE: string;
+  export const REDIRECT_URI: string;
+  export const SCOPE: string;
 }
