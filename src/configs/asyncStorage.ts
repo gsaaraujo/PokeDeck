5d20da004f9f43d73cdbe5efa9fafe52ba0936c5
Gabriel Araujo <gabriel.houth@gmail.com>
2021-07-29 22:23:29 -0300

    feat: add storage config for cards collection

--- src/configs/asyncStorage.ts
+++ src/configs/asyncStorage.ts
@@ -2,4 +2,5 @@ const DATABASE_NAME = '@pokedeck';
 
 export const USER = `${DATABASE_NAME}:user`;
 export const ACCESS_TOKEN = `${DATABASE_NAME}:access_token`;
+export const POKEMON_DECKS = `${DATABASE_NAME}:pokemon_decks`;
 export const POKEMON_CARDS = `${DATABASE_NAME}:pokemon_cards`;
