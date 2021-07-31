d3eb69455e052e4b78da4828cf3a7641a972455a
Gabriel Araujo <gabriel.houth@gmail.com>
2021-07-30 23:07:29 -0300

    feat: add all pokemon types

---
+++ src/utils/pokemonTypes.ts
@@ -0,0 +1,37 @@
+export const pokemonTypes = [
+  { id: '1', type: 'normal', color: '#CA98A6', borderColor: '#75525C' },
+
+  { id: '2', type: 'poison', color: '#9B69DA', borderColor: '#5E2D89' },
+
+  { id: '3', type: 'water', color: '#85A8FB', borderColor: '#1552E1' },
+
+  { id: '4', type: 'rock', color: '#8B3E22', borderColor: '#48190B' },
+
+  { id: '5', type: 'steel', color: '#43BD94', borderColor: '#60756E' },
+
+  { id: '6', type: 'psychic', color: '#FF46A9', borderColor: '#A52A6C' },
+
+  { id: '7', type: 'dark', color: '#595978', borderColor: '#000000' },
+
+  { id: '8', type: 'dragon', color: '#62CAD9', borderColor: '#448A95' },
+
+  { id: '9', type: 'fighting', color: '#EF6239', borderColor: '#994025' },
+
+  { id: '10', type: 'fire', color: '#FF5462', borderColor: '#AB1F24' },
+
+  { id: '11', type: 'electric', color: '#FFDB6F', borderColor: '#F2994A' },
+
+  { id: '12', type: 'flying', color: '#94B2C7', borderColor: '#4A677D' },
+
+  { id: '13', type: 'ghost', color: '#4A677D', borderColor: '#33336B' },
+
+  { id: '14', type: 'grass', color: '#49BE7A', borderColor: '#147B3D' },
+
+  { id: '15', type: 'ground', color: '#6E491F', borderColor: '#A8702D' },
+
+  { id: '16', type: 'ice', color: '#8ADDFF', borderColor: '#30B1ED' },
+
+  { id: '17', type: 'bug', color: '#3C9950', borderColor: '#147B3D' },
+
+  { id: '18', type: 'fairy', color: '#E91368', borderColor: '#961A45' },
+];
