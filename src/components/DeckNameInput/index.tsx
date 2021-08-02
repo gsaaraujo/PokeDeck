8092fad20568fb6fe143db6efb04773e6275e01d
Gabriel Araujo <gabriel.houth@gmail.com>
2021-08-02 15:28:16 -0300

    feat: add type to receive any native props

--- src/components/DeckNameInput/index.tsx
+++ src/components/DeckNameInput/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { TextInputProps } from 'react-native';
 
 import { Container } from './styles';
 
-type Props = {
+type Props = TextInputProps & {
   handleInputText: (name: string) => void;
 };
 
-export const DeckNameInput = ({ handleInputText }: Props) => {
-  return <Container onChangeText={text => handleInputText(text)} />;
+export const DeckNameInput = ({ handleInputText, ...rest }: Props) => {
+  return <Container onChangeText={text => handleInputText(text)} {...rest} />;
 };
