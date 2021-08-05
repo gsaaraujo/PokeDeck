10d38f3b6ee820e01b560f640b1886a3a0d0ff94
Gabriel Araujo <gabriel.houth@gmail.com>
2021-08-05 19:36:25 -0300

    feat: add PokemonDescription screen

---
+++ src/screens/PokemonDescription/index.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import { Keyboard } from 'react-native';
+
+import { SvgUri } from 'react-native-svg';
+
+import { UserCard } from '../../context/deck';
+import { theme } from '../../global/styles/theme';
+
+import { CloseModalButton } from '../../components/CloseModalButton';
+
+import {
+  Container,
+  Content,
+  PokemonContent,
+  Text,
+  Characteristic,
+  Line,
+} from './styles';
+import { PokemonAttribute } from '../../components/PokemonAttribute';
+
+type Props = {
+  cardInfo: UserCard;
+  handleIsModalVisible: () => void;
+};
+
+export const PokemonDescription = ({
+  cardInfo,
+  handleIsModalVisible,
+}: Props) => {
+  const { textColor100 } = theme.colors;
+
+  return (
+    <Container>
+      <CloseModalButton handleIsModalVisible={handleIsModalVisible} />
+
+      <Content>
+        <PokemonContent>
+          <Text color={textColor100} size={18}>
+            {cardInfo.name[0].toLocaleUpperCase() + cardInfo.name.slice(1)}
+          </Text>
+
+          <SvgUri
+            uri={cardInfo.picture}
+            width={150}
+            height={150}
+            style={{ marginVertical: 40 }}
+          />
+
+          <Characteristic>
+            <PokemonAttribute text='weight' subtext={[cardInfo.weight / 10]} />
+            <PokemonAttribute text='height' subtext={[cardInfo.height * 10]} />
+            <PokemonAttribute text='type' subtext={cardInfo.types} />
+          </Characteristic>
+
+          <Line />
+        </PokemonContent>
+      </Content>
+    </Container>
+  );
+};
---
+++ src/screens/PokemonDescription/styles.tsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components/native';
+
+import { theme } from '../../global/styles/theme';
+
+type TextProps = {
+  color: string;
+  size: string[] | number;
+};
+
+export const Container = styled.View`
+  height: 70%;
+  padding: 40px 25px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  background-color: ${theme.colors.background};
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  margin-top: 40px;
+`;
+
+export const PokemonContent = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Text = styled.Text<TextProps>`
+  color: ${props => props.color};
+  font-size: ${props => props.size}px;
+  font-family: ${theme.fonts.textFont50};
+`;
+
+export const Characteristic = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
+
+export const Line = styled.View`
+  width: 100%;
+  height: 2px;
+  margin-top: 25px;
+  margin-bottom: 25px;
+  background-color: ${theme.colors.placeholder};
+`;
